0c825c7f1f4600a7941a7370d985eb881ebdaf5c
Bornstranger <bornstranger40@gmail.com>
2026-07-01 23:43:27 +0530

    feat(billing): connect upgrade modal to checkout endpoint

--- frontend/src/api/billing.js
+++ frontend/src/api/billing.js
@@ -1,4 +1,7 @@
 import api from "./axios";
 
+export const createCheckoutSession = () =>
+  api.post("/billing/create-checkout-session");
+
 export const recordUpgradeInterest = (source = "upgrade_modal") =>
   api.post("/billing/upgrade-interest", { source });
